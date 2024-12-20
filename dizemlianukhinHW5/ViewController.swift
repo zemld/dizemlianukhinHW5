6cd76916d744494138a8e8d8544ea554845061f0
Sunr1seColours <zemlyanukhind@yandex.ru>
2024-12-20 17:50:28 +0300

    Удален сторибоард

---
+++ dizemlianukhinHW5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dizemlianukhinHW5
+//
+//  Created by Denis Zemlyanukhin on 20.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
